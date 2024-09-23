45ef5645fa384cce3fcebb6274ea90f18d4d06fb
la00429 <laura.figuereedo02@uptc.edu.co>
2024-09-22 23:39:52 -0500

    Añadiendo 2 funcionalidad con sus respectivas excepciones

--- src/main/java/co/edu/uptc/animals_rest/controllers/AnimalController.java
+++ src/main/java/co/edu/uptc/animals_rest/controllers/AnimalController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,17 @@ public class AnimalController {
         return animalService.getAnimalInRange(from, to);
     }
 
+    @GetMapping("/category/{category}")
+    public List<Animal> getAnimal(@PathVariable String category) throws IOException {
+        logger.info("getAnimal called with parameters: category = {}", category);
+        return animalService.getAnimalByCategory(category);
+    }
+
+    @GetMapping("/name-length/{length}")
+    public List<Animal> getAnimal(@PathVariable int length) throws IOException {
+        logger.info("getAnimal called with parameters: length = {}", length);
+        return animalService.getAnimalNameLength(length);
+    }
+    
 
 }
--- src/main/java/co/edu/uptc/animals_rest/services/AnimalService.java
+++ src/main/java/co/edu/uptc/animals_rest/services/AnimalService.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import co.edu.uptc.animals_rest.exception.CategoryNotFoundException;
 import co.edu.uptc.animals_rest.exception.InvalidRangeException;
+import co.edu.uptc.animals_rest.exception.InvalideLengthNameException;
 import co.edu.uptc.animals_rest.models.Animal;
 
 import java.util.ArrayList;
@@ -25,12 +27,10 @@ public class AnimalService {
     public List<Animal> getAnimalInRange(int from, int to) throws IOException {
         List<String> listAnimal = Files.readAllLines(Paths.get(filePath));
         List<Animal> animales = new ArrayList<>();
-        
         if (from < 0 || to >= listAnimal.size() || from > to) {
             logger.warn("Invalid range: Please check the provided indices. Range: 0 to {}",listAnimal.size());
              throw new InvalidRangeException("Invalid range: Please check the provided indices.");
         }
-
         for (String line : listAnimal) {
             String[] parts = line.split(",");
             if (parts.length == 2) {
@@ -39,15 +39,12 @@ public class AnimalService {
                 animales.add(new Animal(nombre, categoria));
             }
         }
-    
         return animales.subList(from, to + 1);
     }
 
     public List<Animal> getAnimalAll() throws IOException {
         List<String> listAnimal = Files.readAllLines(Paths.get(filePath));
         List<Animal> animales = new ArrayList<>();
-        
-
         for (String line : listAnimal) {
             String[] parts = line.split(",");
             if (parts.length == 2) {
@@ -55,6 +52,51 @@ public class AnimalService {
                 String name = parts[1].trim();                
                 animales.add(new Animal(name, category));
             }
+        }
+    
+        return animales;
+    }
+
+    public List<Animal> getAnimalByCategory(String categoryFound) throws IOException {
+        List<String> listAnimal = Files.readAllLines(Paths.get(filePath));
+        List<Animal> animales = new ArrayList<>();
+        boolean categoryExists = false;
+
+        for (String line : listAnimal) {
+            String[] parts = line.split(",");
+            if (parts.length == 2 &&parts[0].trim().equals(categoryFound)) {
+                categoryExists = true;
+                String category = parts[0].trim();
+                String name = parts[1].trim();                
+                animales.add(new Animal(name, category));
+                
+            }
+        }
+
+        if (!categoryExists) {
+        throw new CategoryNotFoundException("Category not found: " + categoryFound);
+    }
+    
+        return animales;
+        
+    }
+
+    public List<Animal> getAnimalNameLength(int length) throws IOException {
+        List<String> listAnimal = Files.readAllLines(Paths.get(filePath));
+        List<Animal> animales = new ArrayList<>();
+        boolean lenghtName = false;
+        for (String line : listAnimal) {
+            String[] parts = line.split(",");
+            if (parts.length == 2 && parts[1].trim().length() < length) {
+                lenghtName = true;
+                String category = parts[0].trim();
+                String name = parts[1].trim();                
+                animales.add(new Animal(name, category));
+                }
+        }
+
+        if(!lenghtName){
+            throw new InvalideLengthNameException("No animals were found with names shorter than" + length +" letters");
         }
     
         return animales;
